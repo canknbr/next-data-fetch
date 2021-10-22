f80a159e0273627189b8ea544e283c04e068fb3f
Can Kanbur <cankanbur1@gmail.com>
2021-10-22 08:37:31 +0300

    updated [pid] pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
-
-const HomePage = () => {
+import Link from 'next/link';
+import fs from 'fs/promises';
+import path from 'path';
+const HomePage = ({ products }) => {
   return (
-    <div>
-      <h1>Lorem, ipsum.</h1>
-    </div>
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
-
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-data.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
 export default HomePage;
